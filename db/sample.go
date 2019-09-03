fddc561f06aafb35a0769dd3a4ec4b31d12b9376
6f8l <atsuki02jojo@gmail.com>
2019-09-03 20:53:23 +0900

    Complete GetAll function

--- db/sample.go
+++ db/sample.go
@@ -1,10 +1,14 @@
 package db
 
-import "../schema"
+import (
+	"time"
+
+	"../schema"
+)
 
 type Sample struct{}
 
-func (s *Sample) Colse() {}
+func (s *Sample) Close() {}
 
 func (s *Sample) Insert(todo *schema.Todo) (int, error) {
 	return 0, nil
@@ -15,5 +19,25 @@ func (s *Sample) Delete(id int) error {
 }
 
 func (s *Sample) GetAll() ([]schema.Todo, error) {
-	return nil, nil
+	todoList := []schema.Todo{
+		{
+			ID:      1,
+			Title:   "Do dishes",
+			Note:    "",
+			DueDate: time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC),
+		},
+		{
+			ID:      2,
+			Title:   "Do homework",
+			Note:    "",
+			DueDate: time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC),
+		},
+		{
+			ID:      2,
+			Title:   "Twitter",
+			Note:    "",
+			DueDate: time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC),
+		},
+	}
+	return todoList, nil
 }
